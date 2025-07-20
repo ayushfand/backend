7625daab91b6e655fe260748a2fc85adbbf90d92
Ayush Fand <ayushfand@gmail.com>
2025-07-20 17:39:08 +0530

    added some handlers in utils and created server in app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app= express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // handels data like form submitted 
+app.use(express.static("public")) // Serves static files (HTML, images, CSS, etc.) from the public directory.
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection falied !!!",err);
+})
 
 /*import express from "express";
 const app=express()
