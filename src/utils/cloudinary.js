123bf1c52377c30c388da0a265b1e63ffa4444d3
Ayush Fand <ayushfand@gmail.com>
2025-08-01 00:45:40 +0530

    Added refreshAccessToken

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,6 +17,8 @@ const uploadOnCloudinary =async (localFilePath) => {
         })
          // file has been uploaded on cloudinary
         console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
         
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
